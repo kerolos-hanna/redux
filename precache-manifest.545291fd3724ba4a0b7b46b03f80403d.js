4d4b9488e33aaaf08215a34e601fe1ed324ca62d
kerolos hanna saad <https://github.com/kerolos-hanna>
2020-05-23 15:32:41 +0200

    Updates

---
+++ precache-manifest.545291fd3724ba4a0b7b46b03f80403d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11aef91fc887eb03e1301c02108cb592",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "8dc394c5d390c34222da",
+    "url": "/redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "466c648f963bb68b0c2a",
+    "url": "/redux/static/js/2.d105b73e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux/static/js/2.d105b73e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc394c5d390c34222da",
+    "url": "/redux/static/js/main.f6362970.chunk.js"
+  },
+  {
+    "revision": "b2fd6f2b2b33fc684118",
+    "url": "/redux/static/js/runtime-main.70cd8c53.js"
+  }
+]);
